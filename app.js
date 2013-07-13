002a51441bc3523606c37c01a9735df860cf807b
Eddie Zaneski <eddiezane@gmail.com>
2013-07-13 14:41:20 -0700

    Add opentok template

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , opentok = require('opentok')
+  , opentok_key = process.env.OPENTOK_KEY
+  , opentok_secret = process.env.OPENTOK_SECRET
+  , location = '127.0.0.1';
+
+var ot = new opentok.OpenTokSDK(opentok_key, opentok_secret);
 
 server.listen(8080);
 
 app.use('/assets', express.static('assets'));
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/views/index.html');
+  console.log(ot);
+  var sessionId = '';
+  ot.createSession(location, function(result){
+    sessionId = result;
+    console.log(sessionId);
+    res.sendfile(__dirname + '/views/toktest.html');
+  });
+});
+
+app.get('/lobby', function (req, res) {
+  console.log(ot);
+  var sessionId = '';
+  ot.createSession(location, function(result){
+    sessionId = result;
+    console.log(sessionId);
+    res.sendfile(__dirname + '/views/toktest.html');
+  });
+});
+
+app.get('/room/:id', function (req, res) {
+  res.sendfile(__dirname + '/views/toktest.html');
 });
