9275ba9feac87dc5634c29e312a100784e78bf4c
James Gary <mrjamesgary@gmail.com>
2013-07-14 15:41:59 -0700

    rm unnecessary code

--- assets/js/stateMachine.js
+++ assets/js/stateMachine.js
@@ -17,37 +17,6 @@ stateMachine = {
     $allDivs.hide();
     $roomDiv.show();
   },
-  startBroadcasting: function() {
-    TB.addEventListener("exception", exceptionHandler);
-    var session = TB.initSession("1_MX4zNTA5NDYyMn4xMjcuMC4wLjF-U3VuIEp1bCAxNCAxNDo0MDowOSBQRFQgMjAxM34wLjYxODM1MzR-"); // Replace with your own session ID. See https://dashboard.tokbox.com/projects
-    session.addEventListener("sessionConnected", sessionConnectedHandler);
-    session.addEventListener("streamCreated", streamCreatedHandler);
-    session.connect("35094622",
-    "T1==cGFydG5lcl9pZD0zNTA5NDYyMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz03MjI2ZDk1NDQ5NWI4ZTM3NzA1YTg2MWRmMTJiMDg2ZDRhMjZkYzZkOnJvbGU9bW9kZXJhdG9yJnNlc3Npb25faWQ9MV9NWDR6TlRBNU5EWXlNbjR4TWpjdU1DNHdMakYtVTNWdUlFcDFiQ0F4TkNBeE5EbzBNRG93T1NCUVJGUWdNakF4TTM0d0xqWXhPRE0xTXpSLSZjcmVhdGVfdGltZT0xMzczODM4MDE5Jm5vbmNlPTAuNDQ3Mjg2MzkyMDIzNTYzNyZleHBpcmVfdGltZT0xMzc2NDMwMDE5JmNvbm5lY3Rpb25fZGF0YT0="); // Replace with your API key and token. See https://dashboard.tokbox.com/projects
-                                              // and https://dashboard.tokbox.com/projects
-
-    function sessionConnectedHandler(event) {
-        subscribeToStreams(event.streams);
-        session.publish();
-    }
-    
-    function streamCreatedHandler(event) {
-      subscribeToStreams(event.streams);
-    }
-    
-    function subscribeToStreams(streams) {
-      for (var i = 0; i < streams.length; i++) {
-        var stream = streams[i];
-        if (stream.connection.connectionId != session.connection.connectionId) {
-          session.subscribe(stream);
-        }
-      }
-    }
-    
-    function exceptionHandler(event) {
-      alert("Exception: " + event.code + "::" + event.message);
-    }
-  }
 }
 
 if (location.pathname.match(/room/)) {
