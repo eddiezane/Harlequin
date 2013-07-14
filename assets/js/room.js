a91e65627497628f5c3016767018a95e01d11c68
James Gary <mrjamesgary@gmail.com>
2013-07-14 16:19:50 -0700

    Add hint

--- assets/js/room.js
+++ assets/js/room.js
@@ -8,3 +8,9 @@ socket.on('roomCreated', function(data) {
   var roomId = data.roomId;
   stateMachine.joinRoom(roomId);
 });
+
+socket.on('hint', function(data) {
+  var iframeUrl = data.url;
+  $('.leaderboard').after('<iframe id="ytplayer" type="text/html" width="300" height="200" src="' + iframeUrl + '" frameborder="0" allowfullscreen>');
+  $('.leaderboard').after('<h3>Video hint:</h3>');
+});
